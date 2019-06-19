d77f8560b2376cc88db88dcae4a89274c33c2071
Peter <peter@Peters-MacBook-Pro-2.local>
2019-06-18 23:28:41 -0400

    parts a and b done

--- iss.py
+++ iss.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+import urllib
+
+__author__ = 'peter mayor'
 
 
 def main():
-    pass
+    print('hi')
+    # if not os.path.exists(str(dest_dir)):
+    #     os.mkdir(dest_dir)
+    url_1 = 'http://api.open-notify.org/astros.json'
+    filename_1 = 'astronauts.json'
+    urllib.urlretrieve(url_1, filename=filename_1)
+    with open(filename_1, 'rw') as f:
+        file_object = eval(f.read())
+        for key, value in file_object.iteritems():
+            print key, value
+
+    url_2 = 'http://api.open-notify.org/iss-now.json'
+    filename_2 = 'iss-now.json'
+    urllib.urlretrieve(url_2, filename=filename_2)
+    with open(filename_2, 'rw') as f:
+        file_object = eval(f.read())
+        for key, value in file_object.iteritems():
+            print key, value
 
 
 if __name__ == '__main__':

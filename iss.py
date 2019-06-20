871bf44090de7cac093d26f031c658d2fb98ab51
Peter <peter@Peters-MacBook-Pro-2.local>
2019-06-19 23:02:56 -0400

    docstrings / cleanup

--- iss.py
+++ iss.py
@@ -8,65 +8,78 @@ import turtle
 import time
 
 
-def turtleStart(x, y):
+def turtleStart(lat, lon, time):
+    '''plot indy dot, write passover time, plot iss location from lat,lon'''
+    map_skew_multiplier = 4
+    indy_lat = -40
+    indy_lon = 87
     turtle.screen = turtle.Screen()
     turtle.screen.bgpic("map.gif")
     turtle.screen.register_shape("iss.gif")
     turtle.shape('iss.gif')
-    turtle.screen.update()
 
     turtle.penup()
-    turtle.goto(-40*4, 87)
+    turtle.goto(indy_lat*map_skew_multiplier, indy_lon)
     turtle.dot(10, 'yellow')
 
-    turtle.goto(x*4, y)
+    turtle.write(time, font=("Arial", 16, "bold"))
 
-    turtle.end_fill()
-
+    turtle.goto(lat*map_skew_multiplier, lon)
     turtle.done()
 
 
-def main():
-
-    url_1 = 'http://api.open-notify.org/astros.json'
-    filename_1 = 'astronauts.json'
-    urllib.urlretrieve(url_1, filename=filename_1)
-    with open(filename_1, 'rw') as f:
+def get_astronauts():
+    '''get astronaut data, print it, and create astronauts.json file'''
+    url = 'http://api.open-notify.org/astros.json'
+    filename = 'astronauts.json'
+    urllib.urlretrieve(url, filename=filename)
+    with open(filename, 'rw') as f:
         file_object = eval(f.read())
         for key, value in file_object.iteritems():
             print key, value
 
-    url_2 = 'http://api.open-notify.org/iss-now.json'
-    filename_2 = 'iss-now.json'
-    urllib.urlretrieve(url_2, filename=filename_2)
-    with open(filename_2, 'rw') as f:
+
+def get_iss_location():
+    '''get iss current location and return lat, lon'''
+    url = 'http://api.open-notify.org/iss-now.json'
+    filename = 'iss-now.json'
+    urllib.urlretrieve(url, filename=filename)
+    with open(filename, 'rw') as f:
         file_object = eval(f.read())
         for key, value in file_object.iteritems():
             if key == 'iss_position':
                 iss_lat = float(value['latitude'])
                 iss_long = float(value['longitude'])
+                return iss_lat, iss_long
             else:
                 pass
-            print key, value
 
-    url_3 = 'http://api.open-notify.org/iss-pass.json?lat=' +\
-        str(iss_lat)+'&lon='+str(iss_long)
-    filename_3 = 'iss-pass.json'
-    urllib.urlretrieve(url_3, filename=filename_3)
-    with open(filename_3, 'rw') as f:
+
+def get_indy_passover_time(iss_lat, iss_lon):
+    '''return indy passover time'''
+    url = 'http://api.open-notify.org/iss-pass.json?lat=' +\
+        str(iss_lat)+'&lon='+str(iss_lon)
+    filename = 'iss-pass.json'
+    urllib.urlretrieve(url, filename=filename)
+    with open(filename, 'rw') as f:
         file_object = eval(f.read())
         for key, value in file_object.iteritems():
             print key, value
             if key == 'response':
-                print value
                 for item in value:
-
                     print item
-                    print time.ctime(item['risetime'])
+                    return time.ctime(item['risetime'])
             else:
                 pass
 
-    turtleStart(iss_lat, iss_long)
+
+def main():
+
+    get_astronauts()
+    iss_lat, iss_lon = get_iss_location()
+    time = get_indy_passover_time(iss_lat, iss_lon)
+
+    turtleStart(iss_lat, iss_lon, time)
 
 
 if __name__ == '__main__':

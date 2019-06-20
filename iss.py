89d859a44009fa49efe73488963f972e48c879b0
Peter <peter@Peters-MacBook-Pro-2.local>
2019-06-19 22:43:50 -0400

    close enough

--- iss.py
+++ iss.py
@@ -4,11 +4,30 @@ import urllib
 
 __author__ = 'peter mayor'
 
+import turtle
+import time
+
+
+def turtleStart(x, y):
+    turtle.screen = turtle.Screen()
+    turtle.screen.bgpic("map.gif")
+    turtle.screen.register_shape("iss.gif")
+    turtle.shape('iss.gif')
+    turtle.screen.update()
+
+    turtle.penup()
+    turtle.goto(-40*4, 87)
+    turtle.dot(10, 'yellow')
+
+    turtle.goto(x*4, y)
+
+    turtle.end_fill()
+
+    turtle.done()
+
 
 def main():
-    print('hi')
-    # if not os.path.exists(str(dest_dir)):
-    #     os.mkdir(dest_dir)
+
     url_1 = 'http://api.open-notify.org/astros.json'
     filename_1 = 'astronauts.json'
     urllib.urlretrieve(url_1, filename=filename_1)
@@ -23,7 +42,31 @@ def main():
     with open(filename_2, 'rw') as f:
         file_object = eval(f.read())
         for key, value in file_object.iteritems():
+            if key == 'iss_position':
+                iss_lat = float(value['latitude'])
+                iss_long = float(value['longitude'])
+            else:
+                pass
             print key, value
+
+    url_3 = 'http://api.open-notify.org/iss-pass.json?lat=' +\
+        str(iss_lat)+'&lon='+str(iss_long)
+    filename_3 = 'iss-pass.json'
+    urllib.urlretrieve(url_3, filename=filename_3)
+    with open(filename_3, 'rw') as f:
+        file_object = eval(f.read())
+        for key, value in file_object.iteritems():
+            print key, value
+            if key == 'response':
+                print value
+                for item in value:
+
+                    print item
+                    print time.ctime(item['risetime'])
+            else:
+                pass
+
+    turtleStart(iss_lat, iss_long)
 
 
 if __name__ == '__main__':
